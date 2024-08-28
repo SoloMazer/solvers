e26589733c8890ae030a6838c880fa203fa6bcc6
SoloMazer <solomazer@gmail.com>
2024-08-28 11:32:10 +0530

    cleanup and add comments

--- bisectionMethod/Bisection.cpp
+++ bisectionMethod/Bisection.cpp
@@ -1,21 +1,25 @@
 #include <cmath>
 #include <iostream>
 
-double function(double x) { return std::sin(x) + x * x - 1; }
+// Define target function here
+double f(double x) { return std::sin(x) + x * x - 1; }
 
 int main() {
 
+  // Setting the interval for root findng
   double a{}, b{};
-  double tol{std::numeric_limits<double>::epsilon()};
-  uint n{0};
   std::cout << "Enter lower bound of [a,b]: ";
   std::cin >> a;
   std::cout << "Enter upper bound of [a,b]: ";
   std::cin >> b;
 
-  // Computing initial values of function
-  double f_a{function(a)};
-  double f_b{function(b)};
+  // using machine epsilon as tolerance
+  double tol{std::numeric_limits<double>::epsilon()};
+  uint n{0}; // iterating variable
+
+  // Computing initial values of function to check if bisection can work
+  double f_a{f(a)};
+  double f_b{f(b)};
 
   // Checking for the contions for bisection to work
   if (a >= b) {
@@ -27,28 +31,33 @@ int main() {
     std::cout << "Invalid function in code, does not intersect with x-axis.\n";
     return EXIT_FAILURE;
   } else {
+
+    // iterating a while loop to approach the root
     while (true) {
+
       // computing midpoints
       double mid{(a + b) / 2};
-      double f_mid{function(mid)};
 
-      if (f_mid == 0) { // solution found
-        std::cout << "\n (^_^) \n Solution Found! \n Root is at (" << mid
-                  << ", " << f_mid << ").\n";
+      if (f(mid) == 0) { // solution found
+        std::cout << "Solution Found! \n Root is at (" << mid << ", " << f(mid)
+                  << ").\n";
         return 0;
       } else if (fabs(a - b) < tol) { // Solution beyond tolerance
-        std::cout << "\n (^_^) \n Tolerance contition reached! \n Approximate "
+        std::cout << "Tolerance contition reached! \n Approximate "
                      "Root is at ("
-                  << mid << ", " << f_mid << ").\n";
+                  << mid << ", " << f(mid) << ").\n";
         return 0;
-      } else if (f_mid * f_a < 0) {
+      }
+      // defining the new interval
+      else if (f(mid) * f_a < 0) {
         b = mid;
       } else {
         a = mid;
       }
-      // counting no. of iterations
-      n = n + 1;
-      std::cout << "Iteration: " << n << '\n';
+
+      // computing and printing some useful info
+      n++;
+      std::cout << "i: " << n << " | f(mid): " << f(mid) << '\n';
     }
   }
   return 0;
--- regulaFalsiMethod/regula-falsi.cpp
+++ regulaFalsiMethod/regula-falsi.cpp
@@ -1,41 +1,52 @@
 #include <cmath>
 #include <iostream>
 
+// Define your target function here
 double f(double x) { return std::sin(x) + x * x - 1; }
 
 int main() {
 
+  // setting the interval for root finding
   double a{}, b{};
   std::cout << "Enter the lower bound of [a, b]: ";
   std::cin >> a;
   std::cout << "Enter the upper bound of [a, b]: ";
   std::cin >> b;
 
-  uint n{0};
+  uint n{0}; // iterating variable
+  // set tolerance level to machine epsilon
   double tol{std::numeric_limits<double>::epsilon()};
 
+  // checking existance of root in given interval
   if (f(a) * f(b) >= 0) {
     std::cout << "Invalid bounds, intersection with x-axis is not forced in "
                  "the interval.\n";
     return EXIT_FAILURE;
   } else {
+
+    // iterating a while loop to compute newer intervals
     while (true) {
 
+      // Computing the regula-falsi points
       double x{a - (f(a) * (a - b)) / (f(a) - f(b))};
 
-      if (f(x) == 0) {
+      if (f(x) == 0) { // solution found
         std::cout << "Solution is: ( " << x << ", " << f(x) << ")\n";
         return 0;
-      } else if (fabs(f(x)) < tol) {
-        std::cout << "Approx solution is: ( " << x << ", " << f(x) << ")\n";
+      } else if (fabs(f(x)) < tol) { // tolerance reached
+        std::cout << "\n Approx solution is: ( " << x << ", " << f(x) << ")\n";
         return 0;
-      } else if (f(a) * f(x) < 0) {
+      }
+      // Defining the newer intervals
+      else if (f(a) * f(x) < 0) {
         b = x;
       } else {
         a = x;
       }
+
+      // Printing some useful info
       n++;
-      std::cout << "Iteration: " << n << "\n";
+      std::cout << "i: " << n << " | f(x): " << f(x) << "\n";
     }
   }
   return 0;
